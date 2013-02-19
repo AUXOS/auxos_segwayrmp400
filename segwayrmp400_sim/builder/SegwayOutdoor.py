fc08a9f8296c549ca0c26159faaf1b4f67b1e158
David Hodo <david.hodo@gmail.com>
2013-02-19 16:24:38 -0600

    updated builder script to Morse 1.0

--- segwayrmp400_sim/builder/SegwayOutdoor.py
+++ segwayrmp400_sim/builder/SegwayOutdoor.py
@@ -1,45 +1,30 @@
 #! /usr/bin/env morseexec
 
-import sys
-sys.path.append('/Users/hododav/py3env/lib/python3.2/site-packages')
-
-from morse.builder.morsebuilder import *
-import math
-
+from morse.builder import *
 
 vOff=0.3
 #zRot=math.pi/2 # correct for vehicle axis being y-forward
 zRot=0
 
 # Append ATRV robot to the scene
-segway = WheeledRobot('segwayrmp400_w_em61_trailer')
-segway.properties(Class="SegwayRMP400PhysicsClass", Path="morse/robots/segwayrmp400")
-#segway.translate(z=vOff)
-#segway.rotate(z=-3.0)
-#segway.remove_wheels()
+segway = SegwayRMP400()
+segway.unparent_wheels()
 
 # Append an actuator
-motion = Actuator('vw_diff_drive')
-#motion.translate(z=vOff)
+motion = MotionVWDiff()
 segway.append(motion)
-motion.configure_mw('ros')
 #motion.configure_modifier('NED')
 
 
 # Append an IMU sensor
-imu = Sensor('imu')
+imu = IMU()
 imu.translate(z=0.05)
-#imu.rotate(x=math.pi,z=zRot)
 segway.append(imu)
-imu.configure_mw('ros')
 
 
 # Append a Pose sensor
-pose = Sensor('pose')
-#pose.translate(z=vOff)
-#pose.rotate(z=zRot)
+pose = Pose()
 segway.append(pose)
-pose.configure_mw('ros')
 
 # Append a Wheel Encoder sensor
 #encoders = Sensor('wheel_encoders')
@@ -48,14 +33,11 @@ pose.configure_mw('ros')
 #segway.append(encoders)
 
 # Append a GPS sensor
-gps = Sensor('gps')
-#gps.translate(z=vOff)
-#gps.rotate(z=zRot)
+gps = GPS()
 segway.append(gps)
-gps.configure_mw('ros')
 
-#segway.translate(z=0.2)
 
+segway.add_default_interface('ros')
 
 # Configure the environment
 env = Environment('is4s/large_field')
